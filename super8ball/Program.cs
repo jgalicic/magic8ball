46f6ca1e8246ea3e44cf954d6de31bc8a7f4d5de
Raymond Xu <raymondxu92@gmail.com>
2019-04-03 14:14:29 -0700

    logic to the ball

--- super8ball/Program.cs
+++ super8ball/Program.cs
@@ -1,37 +1,134 @@
 ﻿using System;
+using System.Collections.Generic; 
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
 
-public class Super8ball
+namespace super8Ball
 {
 
-  static void Main()
+
+  public class Super8ball
   {
 
-    System.Console.ForegroundColor = System.ConsoleColor.Cyan;
-    System.Console.BackgroundColor = System.ConsoleColor.Black;
-
-    string text = System.IO.File.ReadAllText(@"C:\Users\jgali\Dropbox\CodingDojo\csharp\super8ball\welcome.txt");
-
-    // Display the file contents to the console. Variable text is a string.
-    System.Console.WriteLine("{0}", text);
-
-    // Keep the console window open in debug mode.
-    System.Console.WriteLine("Please enter your name");
-
-    String s;
-    int ctr = 0;
-    do
+    static void Main(string[] args)
     {
-      ctr++;
-      s = System.Console.ReadLine();
-      System.Console.WriteLine("Welcome to Magic 8 Ball {1}. You are about to recieve your fortune.", ctr, s);
-      System.Console.WriteLine("{0}", text);
-    } while (s != null);
+      ConsoleColor oldColor = Console.ForegroundColor; 
+      System.Console.ForegroundColor = System.ConsoleColor.Cyan;
+      System.Console.BackgroundColor = System.ConsoleColor.Black;
+
+      string welcome = System.IO.File.ReadAllText(@"../welcome.txt");
+      System.Console.WriteLine("{0}", welcome);
 
 
-    System.Console.WriteLine("---");
-    System.Console.WriteLine("Please ask me a question");
+      
+      ProgramName();
+
+      Random randomObject = new Random();
+      while(true)
+      {
+        string questionString = GetQuestionFromUser();
+
+        int numberOfSecondsToSleep = randomObject.Next(2)+1;
+        System.Console.WriteLine("Thinking about your answer, stand by...");
+        Thread.Sleep(numberOfSecondsToSleep * 1000);
+
+        if (questionString.Length == 0)
+        {
+          System.Console.WriteLine("You need to type a question! I can't read minds!");
+          continue;
+        }
+
+        //Exits out of question
+        if (questionString.ToLower() == "quit")
+        {
+          break;
+        }
+        
+        //joke
+        if (questionString.ToLower() == "you suck")
+        {
+          System.Console.WriteLine("You suck even more! Bye!");
+          break;
+        }
+        // System.Console.WriteLine("{0}", randomObject.Next(10)+1);
+        int randomNumber = randomObject.Next(6);
+        //use random numnber to determine response
+        switch(randomNumber)
+        {
+          case 0: 
+            {
+              // System.Console.WriteLine("NullReference Exception");
+              string text = System.IO.File.ReadAllText(@"../1.txt");
+              System.Console.WriteLine("{0}", text);
+              break;
+            }
+          case 1: 
+            {
+              // System.Console.WriteLine("Your questions is very important to us, please wait as I pretend to care.");
+              string text = System.IO.File.ReadAllText(@"../2.txt");
+              System.Console.WriteLine("{0}", text);
+              break;
+            }
+          case 2:
+            {
+              // System.Console.WriteLine("I'm Batmaaaan!");
+              string text = System.IO.File.ReadAllText(@"../3.txt");
+              System.Console.WriteLine("{0}", text);
+              break;
+            }
+          case 3:
+            {
+              // System.Console.WriteLine("Ask again never");
+              string text = System.IO.File.ReadAllText(@"../4.txt");
+              System.Console.WriteLine("{0}", text);
+              break;
+            }
+          case 4:
+            {
+              // System.Console.WriteLine("You were expecting me to say yes or no, weren't you? Surprised you, haven't I?");
+              string text = System.IO.File.ReadAllText(@"../5.txt");
+              System.Console.WriteLine("{0}", text);
+              break;
+            }
+          // case 5:
+          //   {
+          //     System.Console.WriteLine("Out of over 7 billion people on this planet, you decided to ask me?");
+          //     break;
+          //   }
+          // case 6:
+          //   {
+          //     System.Console.WriteLine("I'm literally an ASCII 8 ball. What are you expecting, the answer to life?");
+          //     break;
+          //   }
+          // case 7:
+          //   {
+          //     System.Console.WriteLine("Error 404: Answer not found, try again!");
+          //     break;
+          //   }
+        }
+      }
+      
+      Console.ForegroundColor = oldColor;
+
+    }
+    public static void ProgramName()
+    {
+      System.Console.WriteLine("Magic 8 Ball (by: Justin, Pierre, Raymond)");
+    }
+
+    public static string GetQuestionFromUser()
+    {
+      Console.ForegroundColor = ConsoleColor.White;
+      Console.Write("Ask a question?: ");
+      Console.ForegroundColor = ConsoleColor.Cyan;
+      string questionString = Console.ReadLine();
+
+      return questionString;
+    }
+
+
+
   }
-
-
-
 }
